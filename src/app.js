51fa8c32f575cfa1e9ff19c101284c65ae223f63
jlevigt <jlevi.gt@gmail.com>
2024-07-15 22:12:03 -0300

    add swagger

--- src/app.js
+++ src/app.js
@@ -2,13 +2,17 @@ import express, { json } from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json" assert { type: "json" };
+
 import userRoutes from "./routes/userRoutes.js";
 
 import errorMiddleware from "./middlewares/errorHandlerMiddleware.js";
 
 const app = express();
-
 app.use(json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api/v1/users", userRoutes);
 
